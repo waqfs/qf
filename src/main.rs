96468d8cabfe3a94c231e6ebd14ed597157ad604
waqfs <43045469+waqfs@users.noreply.github.com>
2026-09-09 16:31:18 -0400

    feat: parse command from CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
+#[derive(Debug)]
+enum Command {
+    Build,
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn parse_command() -> Result<Command, String> {
+    let mut arguments = env::args().skip(1);
+    let command_raw = arguments
+        .next()
+        .ok_or_else(|| "Missing command argument.".to_string())?;
+    match command_raw.as_str() {
+        "build" => Ok(Command::Build),
+        _ => Err(format!("Unknown command: {command_raw}")),
+    }
 }
